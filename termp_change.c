cf1f3346aba1e8632c266acc66118862d73388f6
parthattrish <parthattrish@gmail.com>
2025-03-23 22:04:32 +0530

    Create termp_change.c

---
+++ termp_change.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+void main()
+{
+	float f,c;
+	printf("enter the temperature in degree celsius :");
+	scanf("%f",&c);
+	f=(c*1.8)+32;
+	printf("the temperature in fahrenhiet is : %f",f);
+}
