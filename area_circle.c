b62498461a92c81c5a6bf977532b261b5c86ad46
parthattrish <parthattrish@gmail.com>
2025-03-23 21:59:33 +0530

    Create area_circle.c

---
+++ area_circle.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+	int r;
+	float c,a;
+	printf("enter the value of radius of the circle :\n");
+	scanf("%d",&r);
+	c=2*3.14*r;
+	printf("the circumfrence of the circle is :%f \n",c);
+	a=3.14*r*r;
+	printf("Area of the circle is :%f",a);
+}
