82b7571f644d8ec5adea69ae8919a24d7ddd263d
Parth Sharma <parthattrish@gmail.com>
2025-03-25 14:03:05 +0530

    linear_search.c

--- algorithms/linear_search.c
+++ algorithms/linear_search.c
@@ -1,1 +1,29 @@
+#include<stdio.h>
+void main(){
+    int i,n,b,x;
+    printf("enter the number of elements present in the array:");
+    scanf("%d",&n);
+    printf("enter the value to search:");
+    scanf("%d",&x);
+    int a[n];
+    printf("ENTER THE ELEMENTS OF THE ARRAY:");
+    for(i=0;i<n;i++){
+        scanf("%d",&a[i]);
+}
+for(i=0;i<n;i++){
+if(a[i]==x){
+    printf("%d is present in the array at %d",x,i);
+    b=0;
+        break;
+}
+else if(a[i]!=x){
+    b=1;
+}
+
+}
+if(b){
+    printf("THE SEARCHED ELEMENT IS NOT PRESENT IN THE ARRAY:");
+}
+}
+
 
